b3e1cce30c7293e5f88ade965c522a2253489573
RonyAbreu <ronyelison78@gmail.com>
2023-09-06 13:03:40 -0300

    criação da entidade livro

---
+++ src/main/java/ronyelison/ufpb/livraria/entity/Livro.java
@@ -0,0 +1,24 @@
+package ronyelison.ufpb.livraria.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+
+@Entity
+@Table(name = "tb_livro")
+public class Livro implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "autor_id")
+    private Autor autor;
+}
